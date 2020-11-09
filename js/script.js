05f05c528b818d7b7660c3add1642be6ad6bb668
Erickson Powell <ericksonpowell@gmail.com>
2020-11-08 17:22:21 -0800

    started sudo code

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//JS file for timed quiz, u04 homework
+
+
+// THINGS I WILL NEED
+// vars to hold questions/answers
+// clock/timer functions
+// forums for highscore (or prompt)
+// event handler for click (to start)
+// checks for correct/incorrect
+// dynamicly adding/removing questions into DOM
+
+
+// 25 question pool, 5 questions per quiz ** extra **
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
