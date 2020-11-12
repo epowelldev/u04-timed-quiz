29a55a29b661b19149f4b26b9dd74d299b4c093a
Erickson Powell <ericksonpowell@gmail.com>
2020-11-11 16:21:38 -0800

    added HTML structure and stared JS

--- js/script.js
+++ js/script.js
@@ -3,14 +3,45 @@
 
 // THINGS I WILL NEED
 // vars to hold questions/answers
-// clock/timer functions
+var questionList = {
+    //question arrays are as follows: ["Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct Answer"]
+    question0: ["","","","","","",],
+    question1: ["","","","","","",],
+    question2: ["","","","","","",],
+    question3: ["","","","","","",],
+    question4: ["","","","","","",],
+    question5: ["","","","","","",],
+    question6: ["","","","","","",],
+    question7: ["","","","","","",],
+    question8: ["","","","","","",],
+    question9: ["","","","","","",],
+    question10: ["","","","","","",],
+    question11: ["","","","","","",],
+    question12: ["","","","","","",],
+    question13: ["","","","","","",],
+    question14: ["","","","","","",],
+    question15: ["","","","","","",],
+    question16: ["","","","","","",],
+    question17: ["","","","","","",],
+    question18: ["","","","","","",],
+    question19: ["","","","","","",],
+    question20: ["","","","","","",],
+    question21: ["","","","","","",],
+    question22: ["","","","","","",],
+    question23: ["","","","","","",],
+    question24: ["","","","","","",]
+};
+
+console.log(questionList);
+
+// clock/timer functions (setInterval/clearInterval)
 // forums for highscore (or prompt)
 // event handler for click (to start)
 // checks for correct/incorrect
 // dynamicly adding/removing questions into DOM
 
 
-// 25 question pool, 5 questions per quiz ** extra **
+// 25 question pool, 10 random questions per quiz ** extra **
 //
 
 
@@ -28,12 +59,30 @@
 
 
 // WHEN I click the start button
+addEventListener
+
 // THEN a timer starts and I am presented with a question
+starting score 100, time counts down, wrong question -10
+
 // WHEN I answer a question
+click a button
+
 // THEN I am presented with another question
+delete html through DOM, repopulate html through DOM
+
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
+-10 for incorrect
+
 // WHEN all questions are answered or the timer reaches 0
+while time != 0 continue game?
+if time = 0, then end game
+
 // THEN the game is over
+
+
 // WHEN the game is over
+
+
 // THEN I can save my initials and score
+add form for initials for high score
