e61e1aad8f27fe7f713381be278d1a5a7620ec29
Erickson Powell <ericksonpowell@gmail.com>
2020-11-11 18:36:34 -0800

    changed an object of arrays to an array of objects for the question list

--- js/script.js
+++ js/script.js
@@ -3,50 +3,92 @@
 
 // THINGS I WILL NEED
 // vars to hold questions/answers
-var questionList = {
-    //question arrays are as follows: ["Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct Answer"]
-    question0: ["","","","","","",],
-    question1: ["","","","","","",],
-    question2: ["","","","","","",],
-    question3: ["","","","","","",],
-    question4: ["","","","","","",],
-    question5: ["","","","","","",],
-    question6: ["","","","","","",],
-    question7: ["","","","","","",],
-    question8: ["","","","","","",],
-    question9: ["","","","","","",],
-    question10: ["","","","","","",],
-    question11: ["","","","","","",],
-    question12: ["","","","","","",],
-    question13: ["","","","","","",],
-    question14: ["","","","","","",],
-    question15: ["","","","","","",],
-    question16: ["","","","","","",],
-    question17: ["","","","","","",],
-    question18: ["","","","","","",],
-    question19: ["","","","","","",],
-    question20: ["","","","","","",],
-    question21: ["","","","","","",],
-    question22: ["","","","","","",],
-    question23: ["","","","","","",],
-    question24: ["","","","","","",]
-};
+// 25 question pool, 10 random questions per quiz ** extra **
+//question arrays are as follows: ["Question", "Answer1", "Answer2", "Answer3", "Answer4", "Correct Answer"]
+
+
+var questionList = [
+    {
+        question: "What is a good source of protien?", 
+        answers: ["Water", "Rocks", "Air", "Caribou"],
+        correct: 3
+    },
+    {
+        question: "What is the best source of hydration?",
+        answers: ["water", "Coffee", "Pepsi", "Coke"],
+        correct: 0
+    },
+    {
+        question: "What is an element?",
+        answers: ["Green", "Cobalt", "Blue", "White"],
+        correct: 1
+    },
+    {
+        question: "Is JavaScript fun?",
+        answers: ["Sometimes", "No", "Yes", "Fun is relitive"],
+        correct: 3
+    },
+    {
+        question: "Does JS give you a migraine?",
+        answers: ["Absolutely", "Of course", "I'm not a superhero", "I am only human"],
+        correct: true
+    }
+];
+
 
 console.log(questionList);
 
 // clock/timer functions (setInterval/clearInterval)
+var timeScore = 100;
+var timeScoreDisplay = document.querySelector(".time");
+var startQ = document.querySelector("#start-quiz");
+
+function startGame() {
+    document.querySelector(".welcome").classList.remove("active");
+    document.querySelector(".welcome").classList.add("hidden");
+    document.querySelector(".qCard").classList.remove("hidden");
+
+
+    var scoreInterval = setInterval(function() {
+        timeScore--;
+        timeScoreDisplay.textContent = "Time: " + timeScore;
+
+        if(timeScore === 0) {
+            clearInterval(scoreInterval);
+
+        }
+    }, 1000)
+}
+
+console.log(document)
+
 // forums for highscore (or prompt)
 // event handler for click (to start)
+startQ.addEventListener("click", startGame);
+
 // checks for correct/incorrect
+// if `clicked answer` != questonNumber[5]
+//  -10 seconds
+
 // dynamicly adding/removing questions into DOM
 
 
-// 25 question pool, 10 random questions per quiz ** extra **
-//
 
 
+var selectedQuestions = [];
+// 10 random questions per quiz ** extra **
+function pickQuestions() {
+    // randomly # question from list
+    for (let i = 0; i < 10; i++) {
+        var randomQuestion = Math.floor(Math.random() * 24);
+        console.log(randomQuestion);
+        selectedQuestions.push(randomQuestion);
+        //add a check for duplicate questions later
+    }
+}
 
-
+pickQuestions();
+console.log(selectedQuestions);
 
 
 
